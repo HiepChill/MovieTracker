daedc6d9120c262472cabe1dbaa0fe052551ce52
nmthanh31 <nmthanh3107.03@gmail.com>
2024-05-10 21:52:44 +0700

    space detail v2

--- app/src/main/java/com/hyep/movietracker/screens/DetailSpaceActivity.java
+++ app/src/main/java/com/hyep/movietracker/screens/DetailSpaceActivity.java
@@ -1,4 +1,4 @@
-package com.hyep.movietracker;
+package com.hyep.movietracker.screens;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.hyep.movietracker.R;
 
 public class DetailSpaceActivity extends AppCompatActivity {
 
