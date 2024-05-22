bf24fcd45c472770f6ab9af11c4fa708991573f0
nmthanh31 <nmthanh3107.03@gmail.com>
2024-05-22 15:33:05 +0700

    first commit

--- app/src/main/java/com/hyep/movietracker/utils/Utils.java
+++ app/src/main/java/com/hyep/movietracker/utils/Utils.java
@@ -4,7 +4,7 @@ public class Utils {
     public static String API_KEY = "0b9dffdf2cc5ef177909e12da7782207";
     public static String BASE_URL = "https://api.themoviedb.org/3/";
     public static String BASE_IMG_URL = "https://image.tmdb.org/t/p/w500";
-    
+
     public static String LANGUAGE_ENGLISH = "en-US";
     public static String LANGUAGE_VIETNAMESE = "vi-VN";
 
