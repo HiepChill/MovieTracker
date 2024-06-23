14dd5aaee03a8282cb1dd0997f0995c82bc8e3d3
thuypham41 <167019050+thuypham41@users.noreply.github.com>
2024-06-23 15:20:31 +0700

    calendar

--- app/src/main/java/com/hyep/movietracker/screens/Mark_watched.java
+++ app/src/main/java/com/hyep/movietracker/screens/Mark_watched.java
@@ -2,6 +2,7 @@ package com.hyep.movietracker.screens;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -10,10 +11,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.hyep.movietracker.R;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Mark_watched extends AppCompatActivity {
     private EditText editTextDate;
     private ImageView calendarIcon;
+    private int pickedYear, pickedMonth, pickedDay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,18 +32,33 @@ public class Mark_watched extends AppCompatActivity {
 
     private void showDatePickerDialog() {
         Calendar calendar = Calendar.getInstance();
-        int year = calendar.get(Calendar.YEAR);
-        int month = calendar.get(Calendar.MONTH);
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int year, month, day;
+        if (pickedYear != 0 && pickedMonth != 0 && pickedDay != 0) {
+            year = pickedYear;
+            month = pickedMonth;
+            day = pickedDay;
+        }
+        else {
+            year = calendar.get(Calendar.YEAR);
+            month = calendar.get(Calendar.MONTH);
+            day = calendar.get(Calendar.DAY_OF_MONTH);
+        }
 
         DatePickerDialog datePickerDialog = new DatePickerDialog(
                 Mark_watched.this,
+                R.style.my_dialog_theme,
                 (view, year1, monthOfYear, dayOfMonth) -> {
+                    pickedYear = year1;
+                    pickedMonth = monthOfYear;
+                    pickedDay = dayOfMonth;
+
                     // Set the date in the EditText
                     editTextDate.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year1);
                 },
                 year, month, day);
 
+
         datePickerDialog.show();
+
     }
 }
